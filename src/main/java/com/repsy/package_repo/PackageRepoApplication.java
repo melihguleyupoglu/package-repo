6d57cc0eee82edd1f8b09706fead1f05e17d26f7
melihguleyupoglu <melihguleyup@gmail.com>
2025-04-21 22:57:12 +0300

    initial commit

---
+++ src/main/java/com/repsy/package_repo/PackageRepoApplication.java
@@ -0,0 +1,13 @@
+package com.repsy.package_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repsy/package_repo/PackageRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repsy.package_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackageRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
