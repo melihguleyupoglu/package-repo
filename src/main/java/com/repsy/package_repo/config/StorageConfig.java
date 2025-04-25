32d3da9a1947dbe1f9cd73aaaf7471c91c4c1de0
melihguleyupoglu <melihguleyup@gmail.com>
2025-04-25 14:15:50 +0300

    refactor: change location of config dir

--- src/main/java/com/repsy/package_repo/config/StorageConfig.java
+++ src/main/java/com/repsy/package_repo/config/StorageConfig.java
@@ -1,4 +1,4 @@
-package com.repsy.config;
+package com.repsy.package_repo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
